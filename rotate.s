c3f93ae869d7136b90fa7d1fa7f83b01a8877668
Kevin Adler <adler187@gmail.com>
2019-12-18 11:55:35 -0600

    Add program to rotate mem and write to both regs
    This proves that the ram is working and that we're able to talk
    to both registers.

---
+++ rotate.s
@@ -0,0 +1,39 @@
+; simple program for Ben Eater's 6502 computer kit
+; requires 6522 memory mapped to $6000
+; PORTB rotates right from address $00
+; PORTA rotates left  from address $01
+
+  .org $8000
+
+reset:
+  ; set all pins in port B as outputs
+  lda #$ff
+  sta $6002
+
+  ; set all pins in port A as outputs
+  lda #$ff
+  sta $6003
+
+  lda #$05
+  sta $00
+  sta $01
+
+loop:
+  lda $00
+  ror
+  sta $6000
+  sta $00
+
+  lda $01
+  rol
+  sta $6001
+  sta $01
+
+  jmp loop
+
+  .org $fffa
+  .word $0000
+  .word reset ; reset vector
+  .word $0000
+
+
