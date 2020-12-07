9cade3152ecee2d0b1101135b33eafcabd733803
Kevin Adler <adler187@gmail.com>
2020-12-06 22:29:24 -0600

    Update address decode logic and register definitions
    Now RAM takes bottom 32k of address space, except for the top page,
    which is reserved for I/O. Using top 2 address lines of the page (bottom
    nibble) allows for 4 I/O devices which all get access to 64 addresses
    each. The ATF22V10 has more lines which could be used to further
    subdivide the page (I think you could do 16 devices with 16 addresses
    each), but so far that is not needed (I am only using 3 of the 4 I/O
    devices anyway).
    
    At some pont I should fix the address line pin organization on the PLD
    to make it a little more sensible, but it will work for now.

--- registers.inc.s
+++ registers.inc.s
@@ -2,8 +2,52 @@ R1 = $02
 R2 = $04
 R3 = $06
 
-PB1   = $6000
-PA1   = $6001
-DDRB1 = $6002
-DDRA1 = $6003
+; --- VIA 2 ---
+; TODO: Legacy names - use ORB/ORA instead
+PB1   = $7f00
+PA1   = $7f01
 
+ORB1  = $7f00
+ORA1  = $7f01
+DDRB1 = $7f02
+DDRA1 = $7f03
+T1CL1 = $7f04
+T1CH1 = $7f05
+T1LL1 = $7f06
+T1LH1 = $7f07
+T2CL1 = $7f08
+T2CH1 = $7f09
+SR1   = $7f0a
+ACR1  = $7f0b
+PCR1  = $7f0c
+IFR1  = $7f0d
+IER1  = $7f0e
+;ORA21 = $7f0f
+
+; --- VIA 2 ---
+; TODO: Legacy names - use ORB/ORA instead
+PB2   = $7f40
+PA2   = $7f41
+
+ORB2  = $7f40
+ORA2  = $7f41
+DDRB2 = $7f42
+DDRA2 = $7f43
+T1CL2 = $7f44
+T1CH2 = $7f45
+T1LL2 = $7f46
+T1LH2 = $7f47
+T2CL2 = $7f48
+T2CH2 = $7f49
+SR2   = $7f4a
+ACR2  = $7f4b
+PCR2  = $7f4c
+IFR2  = $7f4d
+IER2  = $7f4e
+;ORA22 = $7f4f
+
+; --- ACIA ---
+ACIADTA  = $7f80
+ACIASTS  = $7f81
+ACIACMD  = $7f82
+ACIACTL  = $7f83
